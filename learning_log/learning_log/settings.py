d831d6b98b01d5d70d6ef500c3043bb23d21bb42
DabrowickiWojciech <dabrowicki.wojciech@gmail.com>
2023-05-27 17:46:24 +0200

    27.05.2023 17:46

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my apps
     'learning_logs',
+    'users',
     
     #default django apps
     'django.contrib.admin',
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#My settings
+LOGIN_URL = 'users:login'
+
